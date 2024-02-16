f594ffda15c1e8fedaf7f366f0742519535d4bdc
Avinash Singh <avinashsingh@macbook-air.mynetworksettings.com>
2024-02-16 18:01:50 -0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -10,3 +13,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+export default app;
